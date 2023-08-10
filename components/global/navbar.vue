1cef778febb35d5117045196ee7537146948b762
Diana Ponce <dianap2341@gmail.com>
2023-08-09 23:15:34 -0400

    WIP responsive navbar

---
+++ components/global/navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="nav">
+    <div class="Logo">Logo</div>
+    <div class="nav-list">
+      <ul>
+        <li>Random Story</li>
+        <li>Top Stories</li>
+        <li>Recent Stories</li>
+        <li><v-btn>Create Account</v-btn></li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style>
+li {
+  list-style-type: none;
+}
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+  width: 100%;
+  min-height: 100px;
+}
+.nav-list ul {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  font-size: 20px;
+  gap: 20px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="app-name">Eerie Night Reads</div>
+  </div>
+</template>
+
+<script setup>
+import Navbar from "../components/global/navbar.vue";
+</script>
+<style>
+@import url("https://fonts.googleapis.com/css?family=MedievalSharp");
+.app-name {
+  color: red;
+  font-family: "MedievalSharp";
+  font-size: 50px;
+  text-align: center;
+}
+</style>
